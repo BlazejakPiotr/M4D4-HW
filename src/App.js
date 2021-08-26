19a0bbb1846e8e362e01ab4b4139d57fb536ea60
BlazejakPiotr <piotr.blazejak@yahoo.com>
2021-08-26 16:31:57 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import WarningSign from "./components/WarningSign";
-import SingleBook from "./components/SingleBook";
-import FilterBookList from "./components/FilterBookList";
 import BookList from "./components/BookList";
 
 import Books from "./data/history.json";
@@ -11,11 +8,7 @@ import MyBadge from "./components/MyBadge";
 function App() {
   return (
     <div className="App">
-      <WarningSign message="Hello, this is working" />
-      <MyBadge text="Strive Badge" color="primary" />
-      <SingleBook singleBook={Books[0]} />
       <BookList booklist={Books} />
-      <FilterBookList />
     </div>
   );
 }
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,15 +1,47 @@
-import Card from "react-bootstrap/Card";
+import { Card, Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import { Component } from "react";
+
+const showComments = async (asin) => {
+  try {
+    let response = await fetch(
+      "https://striveschool-api.herokuapp.com/api/comments/" + asin,
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjYyZjJkNTI2MjAwMTViNmRjOTUiLCJpYXQiOjE2MjkyODc5ODMsImV4cCI6MTYzMDQ5NzU4M30.nNXVsU3Xm3rtBjwRiDZJUn8LcZawq-ItVaCq0cX5GZs",
+        },
+      }
+    );
+    const comments = await response.json();
+    return comments;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 const SingleBook = ({ singleBook }) => (
-  <Card style={{ width: "18rem" }} key={singleBook.asin}>
-    <Card.Img variant="top" src={singleBook.img} />
-    <Card.Body>
-      <Card.Title>{singleBook.title}</Card.Title>
+  <Container>
+    <Row>
+      <Col md={4}>
+        <Card style={{ width: "18rem" }} key={singleBook.asin}>
+          <Card.Img
+            variant="top"
+            src={singleBook.img}
+            onClick={() => showComments(singleBook.asin)}
+          />
+          <Card.Body>
+            <Card.Title>{singleBook.title}</Card.Title>
 
-      <Button variant="primary">Go somewhere</Button>
-    </Card.Body>
-  </Card>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <ul>
+          <li>{showComments[0].author}</li>
+        </ul>
+      </Col>
+    </Row>
+  </Container>
 );
 export default SingleBook;
